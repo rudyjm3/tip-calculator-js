cd2fb5a8faa7afea8ffb18d123c70c73c00e5411
rudyjm3 <rudyjm3@yahoo.com>
2020-11-17 07:28:56 -0500

    fixed decimal issues, correct rounding issues. starting on styling next.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-
+// TAKES CARE OF THE DECIMAL (2 SPACES) AND ROUNDING THE NUMBER PROPERLY
 function calculateTotal () {
   const format = (num, decimals) => num.toLocaleString('en-US', {
    minimumFractionDigits: 2,
@@ -10,11 +10,14 @@ function calculateTotal () {
   let percent = document.querySelector('#tip-percent').value;
   let totalGuest = document.querySelector('#number-of-people').value;
   //let tipAmount = (billAmount * (percent / 100));
-  let tipAmount = (billAmount * (percent * .01));
-  let totalBill = (+billAmount + +tipAmount);
-  let splitAmount = (totalBill / totalGuest);
+  let tipAmount = format(billAmount * (percent * .01));
+  let totalBill = format(+billAmount + +tipAmount);
+  let splitAmount = format(totalBill / totalGuest);
 
-
+// OUT PUT VALLUES //
+  document.getElementById('tip-amount').innerHTML = "Tip amount = $" + tipAmount;
+  document.getElementById('total-amount').innerHTML = "Total bill amount = $" + totalBill;
+  document.getElementById('split-amount').innerHTML = "Per guest amount = $" + splitAmount;
 
   console.log(billAmount);
   console.log(percent);
